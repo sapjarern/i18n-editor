90d65626dc9532d3da1868b1402e924a15214e4b
Sapjarern <sapjarern081@gmail.com>
2024-03-28 12:55:05 +0000

    Update export api for select language

--- apis/translation/views/v1/project.py
+++ apis/translation/views/v1/project.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from apis.translation.models import Project, Translate
@@ -21,13 +22,20 @@ class ProjectViewset(ModelViewSet):
         return super().get_serializer_class()
     
     @action(methods=["GET"], detail=True)
-    def export(self, request, pk, *args, **kwargs):
-        project = self.get_object()
+    def export(self, request: Request, pk, *args, **kwargs):
+        project: Project = self.get_object()
+        language = request.query_params.get('language')
+        language = project.language.filter(code=language).first()
         context = {}
-        print(f'{len(Translate.objects.filter(language__in=project.language.all(), translation_key__project=project)) = }')
-        for translate in Translate.objects.filter(language__in=project.language.all(), translation_key__project=project):
-            if translate.language.code not in context:
-                context.update({translate.language.code: {translate.translation_key.key: translate.translate}})
-            else:
-                context[translate.language.code].update({translate.translation_key.key: translate.translate})
+        if language:
+            queryset = Translate.objects.filter(language=language, translation_key__project=project)
+            for translate in queryset:
+                context.update({translate.translation_key.key: translate.translate})
+        else:
+            queryset = Translate.objects.filter(language__in=project.language.all(), translation_key__project=project)
+            for translate in queryset:
+                if translate.language.code not in context:
+                    context.update({translate.language.code: {translate.translation_key.key: translate.translate}})
+                else:
+                    context[translate.language.code].update({translate.translation_key.key: translate.translate})
         return Response(context, status=status.HTTP_200_OK)
