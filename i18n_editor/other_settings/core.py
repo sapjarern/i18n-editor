382d562c7654a1fe57e9fd99eb06645bc23b1c28
Sapjarern <sapjarern081@gmail.com>
2024-03-29 07:00:49 +0000

    Update setting

--- i18n_editor/other_settings/core.py
+++ i18n_editor/other_settings/core.py
@@ -1,11 +1,14 @@
 import os
+from distutils.util import strtobool
 from corsheaders.defaults import default_headers
 
-CORS_ALLOWED_ORIGIN_REGEXES=''
-CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOWED_ORIGIN_REGEXES=os.getenv('CORS_ALLOWED_ORIGIN_REGEXES', '').split(',')
+CORS_ALLOW_ALL_ORIGINS=bool(strtobool(os.getenv('CORS_ALLOW_ALL_ORIGINS', 'True')))
+CUSTOM_HEADERS=os.getenv('CUSTOM_HEADERS', '').split(',')
 
 CORS_ALLOW_HEADERS = (
     *default_headers,
+    *CUSTOM_HEADERS
 )
 
 CSRF_TRUSTED_ORIGINS = os.getenv('', 'localhsot').split(',')
